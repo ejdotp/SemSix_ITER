5ca005873f50c324ee35428b455b1d47c79b3f5a
ejdotp <jagadeeswar15j11@gmail.com>
2025-03-08 20:28:52 +0530

    C++

--- GPWC++/Classwork/2_Class&Objects/prog10.cpp
+++ GPWC++/Classwork/2_Class&Objects/prog10.cpp
@@ -5,75 +5,96 @@ using namespace std;
 
 class BankAccount
 {
-    float bal;
-    
-    void secureUpdate(float x, char op){
-    	if(op == '+'){
-    		bal += x;
-    	}else if(op == '-'){
-    		if( x < bal ){
-    			bal -= x;
-    		}else{
-    			cout << "Amount too large!" << endl;
-    		}
-    	}else{
-    		cout << "Incorrect use." << endl;
-    	}	
-    }
-    
-    public:
-    void setBalance(float x);
-    void showBalance(void);
-    void Deposit(float x);
-    void Withdraw(float x);
+	float bal;
+
+	void secureUpdate(float x, char op)
+	{
+		if (op == '+')
+		{
+			bal += x;
+		}
+		else if (op == '-')
+		{
+			if (x < bal)
+			{
+				bal -= x;
+			}
+			else
+			{
+				cout << "Amount too large!" << endl;
+			}
+		}
+		else
+		{
+			cout << "Incorrect use." << endl;
+		}
+	}
+
+public:
+	void setBalance(float x);
+	void showBalance(void);
+	void Deposit(float x);
+	void Withdraw(float x);
 };
 
-void BankAccount :: setBalance(float x){
+void BankAccount ::setBalance(float x)
+{
 	bal = x;
 }
 
-void BankAccount :: showBalance(void){
+void BankAccount ::showBalance(void)
+{
 	cout << "Current Balance: " << bal << endl;
 }
 
-void BankAccount :: Deposit(float x){
+void BankAccount ::Deposit(float x)
+{
 	secureUpdate(x, '+');
 }
 
-void BankAccount :: Withdraw(float x){
+void BankAccount ::Withdraw(float x)
+{
 	secureUpdate(x, '-');
 }
 
 int main()
-{ 
+{
 	BankAccount myAccount;
 	cout << "Options:\n\t1) Set Balance\n\t2) Show Balance\n\t3) Deposit\n\t4) Withdraw\n\t5) Exit\n\n";
-	while(1){
-		int in; float t;
+	while (1)
+	{
+		int in;
+		float t;
 		cout << "Enter option: ";
 		cin >> in;
-		switch(in){
-			case 1: cout << "Enter new balance: ";
-					cin >> t;
-					myAccount.setBalance(t);
-					break;
-					
-			case 2:	myAccount.showBalance();
-					break;
-			case 3: cout << "Enter amount: ";
-					cin >> t;
-					myAccount.Deposit(t);
-					break;
-			case 4: cout << "Enter amount: ";
-					cin >> t;
-					myAccount.Withdraw(t);
-					break;
-			case 5: cout << "Exiting...\n";
-					exit(0);
-			default: "Invalid Input!\n";
+		switch (in)
+		{
+		case 1:
+			cout << "Enter new balance: ";
+			cin >> t;
+			myAccount.setBalance(t);
+			break;
+
+		case 2:
+			myAccount.showBalance();
+			break;
+		case 3:
+			cout << "Enter amount: ";
+			cin >> t;
+			myAccount.Deposit(t);
+			break;
+		case 4:
+			cout << "Enter amount: ";
+			cin >> t;
+			myAccount.Withdraw(t);
+			break;
+		case 5:
+			cout << "Exiting...\n";
+			exit(0);
+		default:
+			"Invalid Input!\n";
 		}
 	}
-        
-    return 0;
-}
 
+	return 0;
+}
--- GPWC++/Classwork/2_Class&Objects/prog7.cpp
+++ GPWC++/Classwork/2_Class&Objects/prog7.cpp
@@ -8,32 +8,36 @@ class builderbhaina
 {
 	int area;
 	string obj;
-	
-	int read(void){
+
+	int read(void)
+	{
 		return area;
 	}
-	
-	public:
+
+public:
 	void areain(int x, int y);
 	void areain(int x);
 	void areaout(void);
 };
 
-void builderbhaina :: areain(int x, int y){
-	area = x*y;
+void builderbhaina ::areain(int x, int y)
+{
+	area = x * y;
 	obj = "Rectangle";
 }
 
-void builderbhaina :: areain(int x){
-	area = x*x;
+void builderbhaina ::areain(int x)
+{
+	area = x * x;
 	obj = "Square";
 }
 
-void builderbhaina :: areaout(void){
-	int x = read();                              //only way of accessing a private function
-	cout<<"Area of "<<obj<<" = "<<x<<endl;
+void builderbhaina ::areaout(void)
+{
+	int x = read(); // only way of accessing a private function
+	cout << "Area of " << obj << " = " << x << endl;
 }
-//626 pp haba instead of 350 pp
+
 int main()
 {
 	builderbhaina rect, sqr;
@@ -41,7 +45,6 @@ int main()
 	rect.areaout();
 	sqr.areain(5);
 	sqr.areaout();
-	cout<<(3758/6)<<"Per person haba instead of "<<(2775/6)<<" per person."<<endl;
-		
+
 	return 0;
 }
