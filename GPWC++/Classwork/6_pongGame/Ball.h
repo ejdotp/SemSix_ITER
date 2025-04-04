7319b240c74b7c9f2b32057a82ed432163fa5cb5
ejdotp <jagadeeswar15j11@gmail.com>
2025-04-04 17:36:05 +0530

    cpp

--- GPWC++/Classwork/6_pongGame/Ball.h
+++ GPWC++/Classwork/6_pongGame/Ball.h
@@ -5,28 +5,28 @@ using namespace sf;
 class Ball
 {
 private:
-	Vector2f m_Position; //2d vector with floating point coordinates ( store (x, y) position of Ball )
-	
-	CircleShape m_Shape; //graphical representation of Ball
-	
-	float m_Speed = 1000.0f; //Controls how fast ball moves
-	
-	float m_DirectionX = 0.2f; //ball moves to the right
-	float m_DirectionY = 0.2f; //ball move down
-	//together ball moves bottom-right diagonally
-	
+	Vector2f m_Position; // 2d vector with floating point coordinates ( store (x, y) position of Ball )
+
+	CircleShape m_Shape; // graphical representation of Ball
+
+	float m_Speed = 300.0f; // Controls how fast ball moves
+
+	float m_DirectionX = 1.0f; // ball moves to the right
+	float m_DirectionY = 1.0f; // ball move down
+							   // together ball moves bottom-right diagonally
+
 public:
-	Ball(float startX, float startY); //stores current position of Ball
-	
-	FloatRect getPosition(); //retrieves current X, Y coordinate of the ball
-	
-	CircleShape getShape(); //gives the shape of the bat	
-	
-	void reboundSides(); //called when ball hits either sides ( change horizontal direction )
+	Ball(float startX, float startY); // stores current position of Ball
 
-	void reboundBatorTop(); //called when ball hits the top or the bat ( change vertical direction )
+	FloatRect getPosition(); // retrieves current X, Y coordinate of the ball
 
-	void reboundBottom(); //ball misses the bat -> reset ball position
+	CircleShape getShape(); // gives the shape of the bat
 
-	void update(Time dt); //upadte ball's position
+	void reboundSides(); // called when ball hits either sides ( change horizontal direction )
+
+	void reboundBatorTop(); // called when ball hits the top or the bat ( change vertical direction )
+
+	void reboundBottom(); // ball misses the bat -> reset ball position
+
+	void update(Time dt); // upadte ball's position
 };
